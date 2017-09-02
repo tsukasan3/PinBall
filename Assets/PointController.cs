e7663fa3e3b2e53a67ab23785b2e715529b48c4f
大原聖司 <aterm.0513@gmail.com>
2017-09-02 21:02:17 +0900

    得点表示とマルチタップ対応
    得点表示：画面右上に得点表示。雲と星に玉が衝突した時に、得点が加算されるようにしました。
    マルチタップ対応：スマホの画面右半分のタップ、左半分のタップそれぞれに左右のフリッパーが対応するようにしました。

---
+++ Assets/PointController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointController : MonoBehaviour {
+
+	//ポイントを表示するテキスト
+	private GameObject pointText;
+	
+	//scoreの初期化
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のGameOverTextオブジェクトを取得
+		this.pointText = GameObject.Find("PointText");
+		//ポイントの初期値を表示
+		this.pointText.GetComponent<Text>().text = "Score: 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	//各ターゲットにボールが衝突した時に得点加算
+		void OnCollisionEnter(Collision other){
+			if(other.gameObject.tag == "SmallStarTag"){
+				//小さい星に衝突した時10点加算
+				score += 10;
+			}else if(other.gameObject.tag == "LargeStarTag"){
+				//大きい星に衝突した時20点加算
+				score += 20;
+			}else if(other.gameObject.tag == "SmallCloudTag" ){
+				//小さい雲に衝突した時15点加算
+				score += 15;
+			}else if(other.gameObject.tag == "LargeCloudTag"){
+				//大きい雲に衝突した時50点加算
+				score += 50;
+			}
+			//現在の得点を表示
+			this.pointText.GetComponent<Text>().text = "Score: " + score.ToString();
+		}
+		
+}
---
+++ Assets/TouchTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour {
+	
+	//HingeJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+	
+	//初期の傾き
+	private float defaultAngle = 20;
+	//弾いた時の傾き
+	private float flickAngle = -20;
+
+
+	// Use this for initialization
+	void Start () {
+		//HingeJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint>();
+		
+		//フリッパーの傾きを設定
+		SetAngle(this.defaultAngle);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(Touch touch in Input.touches){ 
+		
+		//画面左側をタップした時左フリッパーを動かす
+		if(touch.phase == TouchPhase.Began && tag =="LeftFripperTag" && touch.position.x <= Screen.width * 0.5f){
+			SetAngle(this.flickAngle);
+		}
+		//画面右側をタップした時右フリッパーを動かす
+		if(touch.phase == TouchPhase.Began && tag =="RightFripperTag" && touch.position.x > Screen.width * 0.5f){
+			SetAngle(this.flickAngle);
+		}
+		
+		//画面から指が離された時フリッパーを元に戻す
+		if(touch.phase == TouchPhase.Ended && tag =="LeftFripperTag" && touch.position.x <= Screen.width * 0.5f){
+			SetAngle(this.defaultAngle);
+		}
+		if(touch.phase == TouchPhase.Ended && tag =="RightFripperTag" && touch.position.x > Screen.width * 0.5f){
+			SetAngle(this.defaultAngle);
+		}
+	}
+	}
+	
+	//フリッパーの傾きを設定
+		public void SetAngle(float angle){
+			JointSpring jointSpr = this.myHingeJoint.spring;
+			jointSpr.targetPosition = angle;
+			this.myHingeJoint.spring = jointSpr;
+		}
+}
